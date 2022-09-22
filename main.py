a12876c20e269ccc030c1a1c55919982b8961244
cjj5210 <57928723+cjj5210@users.noreply.github.com>
2022-09-22 23:54:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 import os
 import random
 import time
+import datetime
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
@@ -44,6 +45,10 @@ def get_birthday2():
     next = next.replace(year=next.year + 1)
   return (next - today).days
 
+def get_nowData():
+  now = datetime.datetime.now().strftime('%Y-%m-%d')
+  return now
+
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
@@ -58,6 +63,6 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"birthday_left2":{"value":get_birthday2()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"nowData":{"value":get_nowData()},"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"birthday_left2":{"value":get_birthday2()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
