615afe8c2b2f95b92dc7cb8bcf25935c31e09828
cjj5210 <57928723+cjj5210@users.noreply.github.com>
2022-09-26 00:21:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from datetime import date, datetime
-from dateutil.relativedelta import relativedelta
 import math
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
@@ -18,8 +17,7 @@ app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
-today1 = datetime.now() - relativedelta(hours=8)
-nowDate = datetime.strftime(today1,'%Y年%m月%d %H:%M:%S')
+nowDate = datetime.strftime(today,'%Y年%m月%d')
 
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
